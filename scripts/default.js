81a5881f47a103b53f53a205b11912ce7650dd21
IOPjolnir <i.o.storozhev990@gmail.com>
2023-04-27 01:33:50 +0500

    Добавил футер и кнопки скроллинга

--- scripts/default.js
+++ scripts/default.js
@@ -1,4 +1,10 @@
 const headerLetters = document.getElementsByClassName('logo-letter');
 
 for (let i = 0; i < headerLetters.length; i++)
-	headerLetters[i].setAttribute("style", "fill:#1E1F1D;fill-opacity:1;fill-rule:nonzero;stroke:none");
+	headerLetters[i].setAttribute('style', 'fill:#1E1F1D;fill-opacity:1;fill-rule:nonzero;stroke:none');
+
+const backBtn = document.getElementById("back-btn");
+
+window.addEventListener('scroll', () => {
+	backBtn.style.bottom = scrollY > 10 ? '10px' : '-60px';
+});
