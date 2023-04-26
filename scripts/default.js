103f69656eac0c251707ef32dd4d2163882f8f6e
IOPjolnir <i.o.storozhev990@gmail.com>
2023-04-26 14:36:12 +0500

    first commit

---
+++ scripts/default.js
@@ -0,0 +1,4 @@
+const headerLetters = document.getElementsByClassName('logo-letter');
+
+for (let i = 0; i < headerLetters.length; i++)
+	headerLetters[i].setAttribute("style", "fill:#1E1F1D;fill-opacity:1;fill-rule:nonzero;stroke:none");
