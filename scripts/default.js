b300939616eb9d072eb64bc58f925218c6fb958b
IOPjolnir <i.o.storozhev990@gmail.com>
2023-04-27 12:46:34 +0500

    Реализован адаптивный дизайн базовой страницы

--- scripts/default.js
+++ scripts/default.js
@@ -3,8 +3,20 @@ const headerLetters = document.getElementsByClassName('logo-letter');
 for (let i = 0; i < headerLetters.length; i++)
 	headerLetters[i].setAttribute('style', 'fill:#1E1F1D;fill-opacity:1;fill-rule:nonzero;stroke:none');
 
-const backBtn = document.getElementById("back-btn");
+const backBtn = document.getElementById('back-btn');
 
 window.addEventListener('scroll', () => {
 	backBtn.style.bottom = scrollY > 10 ? '10px' : '-60px';
+});
+
+const burgerBtn = document.getElementById('burger-logo');
+const sideNav = document.getElementById('side-nav');
+const closeBtn = document.getElementById('side-close');
+
+burgerBtn.addEventListener('click', () => {
+	sideNav.style.right = '0';
+});
+
+closeBtn.addEventListener('click', () => {
+	sideNav.style.right = '-500px';
 });
